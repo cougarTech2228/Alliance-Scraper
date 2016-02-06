83fdc8d64116b853a24118196ea6611e04ff354e
Zachary DuBois <me@zacharydubois.me>
2016-02-06 11:27:05 -0500

    Add some build comments.

--- build/build.php
+++ build/build.php
@@ -15,11 +15,16 @@ if (!isset($argv[1]) || !isset($argv[2])) {
     throw new Exception("Required parameters not set.");
 }
 
+echo "Creating teamid.json..." . PHP_EOL;
 if (!file_put_contents(SRC_PATH . 'teamid.json', json_encode(array('id' => $argv[1], 'url' => $argv[2])))) {
     throw new Exception("Failed to write teamid.json");
 }
 
-
+echo "Creating phar..." . PHP_EOL;
 $phar = new Phar(BUILD_PATH . 'Alliance-Scraper.phar');
+echo "Building..." . PHP_EOL;
 $phar->buildFromDirectory(SRC_PATH);
+echo "Setting defualt stub..." . PHP_EOL;
 $phar->setStub($phar->createDefaultStub('main.php'));
+
+echo "Done." . PHP_EOL;
