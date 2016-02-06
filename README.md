1ed717786906cf18f53c4e591c6f5fdd3256c8f7
Zachary DuBois <me@zacharydubois.me>
2016-02-05 21:02:40 -0500

    Add Config.

--- README.md
+++ README.md
@@ -15,11 +15,12 @@ To run, first build a copy of the code and build it or download the pre-built ph
 
 Run the script in `/build` by executing
 
-`php build.php [team id]`
+`php build.php [team id (FRC2228)] [team website (https://mycoolteam.com)]`
 
 Note: The that the team ID is required because The Blue Alliance API requires it for API calls.
 
 ## Requirements
 
 - `php 5.6 or higher` (not tested with 7).
+- `php5-curl` (or `php7-curl`).
 - An internet connection (obviously).
---
+++ src/app/controller/TConfig.php
@@ -0,0 +1,102 @@
+<?php
+/**
+ * File: TConfig.php
+ * User: zacharydubois
+ * Date: 2016-02-05
+ * Time: 20:26
+ * Project: Alliance-Scraper
+ */
+
+namespace alliance;
+
+
+/**
+ * Class TConfig
+ *
+ * Team configuration handler.
+ * Pulls information from PATH . 'teamid.json'
+ *
+ * @package alliance
+ */
+class TConfig {
+    private
+        $config;
+
+    const path = PATH . 'teamid.json';
+
+    /**
+     * TConfig constructor.
+     *
+     * Initializes $this->config with $this->reload().
+     *
+     * @throws Exception
+     */
+    public function __construct() {
+        $this->reload();
+    }
+
+    /**
+     * Reload from Disk
+     *
+     * Reloads the team configuration from the disk.
+     *
+     * @return true
+     * @throws Exception
+     */
+    private function reload() {
+        $this->config = json_decode(File::read(static::path), true);
+
+        if (!is_array($this->config)) {
+            throw new Exception("Decoded team configuration is not an array.");
+        }
+
+        return true;
+    }
+
+    /**
+     * Read Value from Key
+     *
+     * Reads input key's value in the team configuration file.
+     *
+     * @param string $key
+     * @return string
+     * @throws Exception
+     */
+    public function read($key) {
+        if (!is_string($key)) {
+            throw new Exception("Input config key is not string: " . $key);
+        }
+
+        if (!in_array($key, $this->config) || $this->config[$key] === null) {
+            throw new Exception("Config key is not set: " . $key);
+        }
+
+        return $this->config[$key];
+    }
+
+    /**
+     * Write TConfig to Disk
+     *
+     * Encodes and writes team configuration to the disk.
+     *
+     * @param string $key
+     * @param string $value
+     * @return true
+     * @throws Exception
+     */
+    public function write($key, $value) {
+        if (!is_string($key) || !is_string($value)) {
+            throw new Exception("Config key or value for write are not strings: " . $key . " Value: " . $value);
+        }
+
+        $this->config[$key] = $value;
+
+        if (!File::write(static::path, json_encode($this->config, JSON_PRETTY_PRINT))) {
+            throw new Exception("Failed to write new config to file.");
+        }
+
+        $this->reload();
+
+        return true;
+    }
+}
